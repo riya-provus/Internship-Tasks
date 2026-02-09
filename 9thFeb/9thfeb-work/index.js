92b62bad960d692284a85191b640ea5823770725
riya-provus <riya@provus.ai>
2026-02-09 15:44:47 +0530

    Typescript concepts (fundamentals)

--- 9thFeb/9thfeb-work/index.js
+++ 9thFeb/9thfeb-work/index.js
@@ -1,3 +1,56 @@
 console.log("Hello");
+//static typing with union types
 var data = "riya";
 console.log(data);
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+var info = 78;
+console.log(typeof arr);
+//Dynamic typing with union types
+var data_d = "riya";
+console.log(data);
+var arr_d = [1, 2, 3, 4, 5];
+console.log(arr);
+var info_d = undefined;
+console.log(typeof info_d);
+//typeof always follows javascript rules at the runtime so it is not impacted by static typing 
+//Conditional chaining
+var obj;
+obj = {
+    age: 23,
+    name: "riya",
+};
+console.log(obj);
+//index signature (for any unexpected parameter at the time of API fetching)
+var obj1;
+obj1 = {
+    age: 23,
+    name: "riya",
+};
+obj1.gender = "female";
+console.log(obj1 === null || obj1 === void 0 ? void 0 : obj1.gender);
+//(Complex types)non primitives(object,tuples,arrays,interfaces)
+var arr2 = [1, 2];
+arr2.push(3);
+var tup = ["Riya", 23, null];
+tup.push();
+console.log(arr2);
+console.log(tup);
+//any , unknown 
+var information = "riya";
+console.log(information);
+var information1 = "riya";
+if (typeof information1 == "string") {
+    console.log(information1.toLowerCase());
+}
+//void and never 
+function getinfo() {
+    console.log("Hello void");
+}
+console.log(getinfo());
+console.log("Outside Void");
+function getinfo2() {
+    throw new Error("Inside never");
+}
+console.log(getinfo2());
+console.log("Outside Never");
--- 9thFeb/9thfeb-work/index.ts
+++ 9thFeb/9thfeb-work/index.ts
@@ -1,4 +1,74 @@
 console.log("Hello");
 
+//static typing with union types
 let data:string="riya";
 console.log(data);
+let arr:number[]=[1,2,3,4,5];
+console.log(arr);
+let info:number|string|null=78;
+console.log(typeof arr);
+ 
+
+//Dynamic typing with union types
+let data_d="riya";
+console.log(data);
+let arr_d=[1,2,3,4,5];
+console.log(arr);
+let info_d=undefined;
+console.log(typeof info_d);
+
+//typeof always follows javascript rules at the runtime so it is not impacted by static typing 
+
+//Conditional chaining
+let obj:{age:number,name:string,roll_no?:number};
+obj={
+    age:23,
+    name:"riya",
+}
+console.log(obj);
+
+
+//index signature (for any unexpected parameter at the time of API fetching)
+let obj1:{
+    age:number;
+    name:string;
+    roll_no?:number;
+    [key:string]:unknown
+};
+obj1={
+    age:23,
+    name:"riya",
+}
+obj1.gender="female";
+console.log(obj1?.gender);
+
+//(Complex types)non primitives(object,tuples,arrays,interfaces)
+let arr2: number[] = [1, 2];
+arr2.push(3);    
+let tup: [string, number,null] = ["Riya", 23,null];
+tup.push();    
+console.log(arr2);
+console.log(tup);
+
+
+//any , unknown 
+let information:any="riya";
+console.log(information);
+let information1:unknown="riya";
+if(typeof information1 =="string"){
+    console.log(information1.toLowerCase());
+}
+
+
+//void and never 
+function getinfo():void{
+    console.log("Hello void");    
+}
+console.log(getinfo());
+console.log("Outside Void");
+
+function getinfo2():never{
+    throw new Error("Inside never");
+}
+console.log(getinfo2());
+console.log("Outside Never");
